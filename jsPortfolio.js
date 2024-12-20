9bbc929658dd2c67bde628a14010abc473eddb46
JanhaviSrivastava006 <sri.janhavi21@gmail.com>
2024-12-20 23:40:46 +0530

    Add files via upload

---
+++ jsPortfolio.js
@@ -0,0 +1,8 @@
+/*const navEle=document.querySelector(".btn");
+const block=document.querySelector(".change")
+console.log(block);
+console.log(navEle)
+navEle.addEventListener("click",()=>{
+    alert("working");
+    block.style.color="green";
+})*/
